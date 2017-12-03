fcdf4d33b62d22d10e716a6d9f1ba2df457091d1
Victor King'e <vinge1718@gmail.com>
2017-12-03 16:00:45 +0300

    add the getter method for the array list of words

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -2,28 +2,41 @@ import java.io.Console;
 import java.util.List;
 import java.util.ArrayList;
 
-                // VARIABLES
-                //-----------Words to be used during the game
+            // VARIABLES
+            //-----------Words to be used during the game
 
 private static String[] words = {"weather", "program", "computer", "animals", "beverage", "entertaiment" };
 
-                /* - Selecting a word from the array of words above.
-                -- 1. Generate a random number and multiply by the words-array size/length and initialize the result as a primitive integer.
-                -- 2. Step one gives you a random word from the words array and assigns it a variable "word"
-                */
+            /* - Selecting a word from the array of words above.
+            -- 1. Generate a random number and multiply by the words-array size/length and initialize the result as a primitive integer.
+            -- 2. Step one gives you a random word from the words array and assigns it a variable "word"
+            */
 
 private static String word = words[(int) (Math.random() * words.length)];
 
-                /* - Since characters in java strings are immutable (can't reassign/replace with new values). We create a new string of asterics
-                -- 1. Get the length of the random word selected.
-                -- 2. create a new string whose length is based on the randomly selected word.
-                */
+            /* - Since characters in java strings are immutable (can't reassign/replace with new values). We create a new string of asterics
+            -- 1. Get the length of the random word selected.
+            -- 2. create a new string whose length is based on the randomly selected word by creating new characters from the newly parsed string object.
+            */
 
 private static String asterisk = new String(new char[word.length()]).replace("\0", "*");
 
-                //Keeps track of how many tries the user has made
+            //Keeps track of how many tries the user has made and determines which hangman image gets displayed.
+
 private static int count = 0;
 
 public class Hangman{
+    public static void hang(String guess){
 
+    }
+}
+
+            //implementing getter methods for the variables
+
+public String getWord(){
+    return word;
+}
+
+public String[] getWords(){
+    return words;
 }
