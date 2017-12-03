fcaccce076a52aac19c879ed6ca81e4d5f2a8305
Victor King'e <vinge1718@gmail.com>
2017-12-03 19:02:09 +0300

    comment out the asteric test to rid out errors

--- src/test/java/HangmanTest.java
+++ src/test/java/HangmanTest.java
@@ -12,10 +12,16 @@ public class HangmanTest{
     }
 
     @Test
-    public void word_displayWordFromListOfWords_String(){
+    public void word_displayRandomWordFromListOfWords_String(){
         Hangman testHangman = new Hangman();
         String[] testWords = {"weather", "program", "computer", "animals", "beverage", "entertaiment" };
         String expectedOutput = testWords[(int) (Math.random() * testWords.length)].getClass().getName();
         assertEquals(expectedOutput, testHangman.getWord().getClass().getName());
     }
+    /*
+    @Test
+    public void getAsterisk_outPutGuessedLetter_true(){
+        Hangman testHangman = new Hangman();
+        assertEquals(true, testHangman.getAsterisk(asterisk).contains('*');
+    }*/
 }
