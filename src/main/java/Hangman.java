d2ffd1cfdd867bf40973152578081c007cfafd08
Victor King'e <vinge1718@gmail.com>
2017-12-03 19:00:58 +0300

    add the getter functions for the count and asteric variables

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -14,14 +14,14 @@ public class Hangman{
                 -- 1. Generate a random number and multiply by the words-array size/length and initialize the result as a primitive integer.
                 -- 2. Step one gives you a random word from the words array and assigns it a variable "word"
 
-                -- alternatively --
-                int randomNum = (int) (Math.random() * words.length);
-                word = words[randomNum];  
+                -- simply put --
+                int randomWordPosition = (int) (Math.random() * words.length);
+                word = words[randomWordPosition];
                 */
 
     private static String word = words[(int) (Math.random() * words.length)];
 
-                /* - Since characters in java strings are immutable (can't reassign/replace with new values). We create a new string of asterics
+                /* - Since characters in java strings are immutable (can't reassign/replace with new values). We create a new string of astericks
                 -- 1. Get the length of the random word selected.
                 -- 2. create a new string whose length is based on the randomly selected word by creating new characters from the newly parsed string object.
                 */
@@ -32,9 +32,37 @@ public class Hangman{
 
     private static int count = 0;
 
-    public static void hang(String guess){
+                /* HANG FUNCTION
+                -- 1. Introduce an variable to hold the correctly guessed letter correctGuesses
+                -- 2. For loop that compared the guessed characters with each character in the random word
+                */
 
-    }
+    public static void hang(String guess){
+        String correctGuesses = "";
+
+        for (int i = 0; i < word.length(); i++) {
+			if (word.charAt(i) == guess.charAt(0)) {
+				correctGuesses += guess.charAt(0);
+			} else if (asterisk.charAt(i) != '*') {
+				correctGuesses += word.charAt(i);
+			} else {
+				correctGuesses += "*";
+			}
+		}
+
+
+
+        if (asterisk.equals(correctGuesses)) {
+			count++;
+			hangmanImage();
+		} else {
+			asterisk = correctGuesses;
+		}
+		if (asterisk.equals(word)) {
+			System.out.println("Correct! You win! The word was " + word);
+		}
+	}
+
 
 
                 //implementing getter methods for the variables
@@ -47,4 +75,98 @@ public class Hangman{
     public String[] getWords(){
         return words;
     }
+
+    public int getCount(){
+        return count;
+    }
+
+    public String getAsterisk(){
+        return asterisk;
+    }
+
+                //HANGMAN GALLOWS PROGRESSION
+
+    public static void hangmanImage() {
+		if (count == 1) {
+			System.out.println("Wrong guess, try again");
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			System.out.println("___|___");
+			System.out.println();
+		}
+		if (count == 2) {
+			System.out.println("Wrong guess, try again");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("___|___");
+		}
+		if (count == 3) {
+			System.out.println("Wrong guess, try again");
+			System.out.println("   ____________");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   | ");
+			System.out.println("___|___");
+		}
+		if (count == 4) {
+			System.out.println("Wrong guess, try again");
+			System.out.println("   ____________");
+			System.out.println("   |          _|_");
+			System.out.println("   |         /   \\");
+			System.out.println("   |        |     |");
+			System.out.println("   |         \\_ _/");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("   |");
+			System.out.println("___|___");
+		}
+		if (count == 5) {
+			System.out.println("Wrong guess, try again");
+			System.out.println("   ____________");
+			System.out.println("   |          _|_");
+			System.out.println("   |         /   \\");
+			System.out.println("   |        |     |");
+			System.out.println("   |         \\_ _/");
+			System.out.println("   |           |");
+			System.out.println("   |           |");
+			System.out.println("   |");
+			System.out.println("___|___");
+		}
+		if (count == 6) {
+			System.out.println("Wrong guess, try again");
+			System.out.println("   ____________");
+			System.out.println("   |          _|_");
+			System.out.println("   |         /   \\");
+			System.out.println("   |        |     |");
+			System.out.println("   |         \\_ _/");
+            System.out.println("   |          _|_");
+            System.out.println("   |         / | \\");
+			System.out.println("   |           |");
+		}
+		if (count == 7) {
+			System.out.println("GAME OVER!");
+			System.out.println("   ____________");
+			System.out.println("   |          _|_");
+			System.out.println("   |         /   \\");
+			System.out.println("   |        |     |");
+			System.out.println("   |         \\_ _/");
+            System.out.println("   |          _|_");
+            System.out.println("   |         / | \\");
+            System.out.println("   |           |");
+			System.out.println("   |          / \\ ");
+			System.out.println("___|___      /   \\");
+			System.out.println("GAME OVER! The word was " + word);
+		}
+	}
 }
