543488cd9866688dcf9640a3f3358ee9a6a91a5d
Victor King'e <vinge1718@gmail.com>
2017-12-03 11:03:18 +0300

    add the game words and a measn to select one at random

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -1,6 +1,17 @@
 import java.io.Console;
+import java.util.List;
+import java.util.ArrayList;
+
+                //-----------Words to be used during the game
+
+private static String[] words = {"weather", "program", "computer", "animals", "beverage", "entertaiment" };
+
+                /* - Selecting a word from the array of words above.
+                -- 1. Generate a random number and multiply by the words-array size/length and initialize the result as a primitive integer.
+                -- 2. Step one gives you a random word from the words array and assigns it a variable "word"
+                */
+private static String word = words[(int) (Math.random() * words.length)];
 
 public class Hangman{
-    System.out.println("Rules: I will pick a word and you will try to guess either the word or the letters forming the word!");
 
 }
