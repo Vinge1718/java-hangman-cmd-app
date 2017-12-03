a91fcb9ff59c46f9532e1da3c47004489c74a4c5
Victor King'e <vinge1718@gmail.com>
2017-12-03 16:08:10 +0300

    add the test for returning the list of words meant for use in the game

--- src/test/java/HangmanTest.java
+++ src/test/java/HangmanTest.java
@@ -1,7 +1,13 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.List;
+import java.util.ArrayList;
 
 public class HangmanTest{
     @Test
-    
+    public void hang_displayRandomWord_String(){
+        Hangman testHangman = new Hangman();
+        String[] expectedOutput = {"weather", "program", "computer", "animals", "beverage", "entertaiment" };
+        assertEquals(expectedOutput, testHangman.getWords());
+    }
 }
