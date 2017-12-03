662fa45537301c5512b61ecb835e1b00bbba74a2
Victor King'e <vinge1718@gmail.com>
2017-12-03 18:59:37 +0300

    add the while loop to keep the game going until the count hits seven

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,1 +1,18 @@
 import java.io.Console;
+import java.util.Scanner;
+
+public class App{
+    public static void main(String[] args){
+        Scanner keyBoardInput = new Scanner(System.in);
+        Console myConsole = System.console();
+        Hangman hangMan = new Hangman();
+
+        while (hangMan.getCount() < 7 && hangMan.getAsterisk().contains("*")) {
+			System.out.println("Guess any letter in the word");
+			System.out.println(hangMan.getAsterisk());
+			String guess = myConsole.readLine();
+			hangMan.hang(guess);
+		}
+		keyBoardInput.close();
+    }
+}
