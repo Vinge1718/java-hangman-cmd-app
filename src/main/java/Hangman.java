c584fd49c411447ba3a1c2e7bba6fe7cd375328b
Victor King'e <vinge1718@gmail.com>
2017-12-03 13:41:47 +0300

    add the variables responsible for the hangman game

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -2,6 +2,7 @@ import java.io.Console;
 import java.util.List;
 import java.util.ArrayList;
 
+                // VARIABLES
                 //-----------Words to be used during the game
 
 private static String[] words = {"weather", "program", "computer", "animals", "beverage", "entertaiment" };
@@ -10,7 +11,18 @@ private static String[] words = {"weather", "program", "computer", "animals", "b
                 -- 1. Generate a random number and multiply by the words-array size/length and initialize the result as a primitive integer.
                 -- 2. Step one gives you a random word from the words array and assigns it a variable "word"
                 */
+
 private static String word = words[(int) (Math.random() * words.length)];
+
+                /* - Since characters in java strings are immutable (can't reassign/replace with new values). We create a new string of asterics
+                -- 1. Get the length of the random word selected.
+                -- 2. create a new string whose length is based on the randomly selected word.
+                */
+
+private static String asterisk = new String(new char[word.length()]).replace("\0", "*");
+
+                //Keeps track of how many tries the user has made
+private static int count = 0;
 
 public class Hangman{
 
