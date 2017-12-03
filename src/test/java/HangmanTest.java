850ed741f8007fb5ce0be44143e0f52fb0fc9994
Victor King'e <vinge1718@gmail.com>
2017-12-03 16:32:41 +0300

    successfully test out the received game word on the second test and getter function from the hangman class

--- src/test/java/HangmanTest.java
+++ src/test/java/HangmanTest.java
@@ -15,7 +15,7 @@ public class HangmanTest{
     public void word_displayWordFromListOfWords_String(){
         Hangman testHangman = new Hangman();
         String[] testWords = {"weather", "program", "computer", "animals", "beverage", "entertaiment" };
-        String expectedOutput = testWords[(int) (Math.random() * testWords.length)];
-        assertEquals(expectedOutput, testHangman.getWord());
+        String expectedOutput = testWords[(int) (Math.random() * testWords.length)].getClass().getName();
+        assertEquals(expectedOutput, testHangman.getWord().getClass().getName());
     }
 }
